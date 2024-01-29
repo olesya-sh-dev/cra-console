eba7f5536061fa87cceee588f26d54258476cbcd
olesyaShalay <olesyakublickaya@yandex.ru>
2024-01-29 11:42:40 +0300

    the last

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        hello world hello world hello world NEW BRANCH byyyy hello world hello
-        world hello world NEW BRANCH new branch NEW-DEV
+        hello world hello world hello world NEW BRANCH byyyy
       </header>
     </div>
   );
