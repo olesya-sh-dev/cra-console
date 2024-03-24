e1bc59b5c887dae8953a1d5055cc4aa5c889ca92
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-03-24 19:28:31 +0300

    useEffect with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState(0);
+  const valueAsString = localStorage.getItem("counterValue");
+  const initialValue = valueAsString ? JSON.parse(valueAsString) : 0;
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    localStorage.setItem("counterValue", JSON.stringify(value));
+  }, [value]);
+
+  // useEffect(() => {
+  //   let valueAsString = localStorage.getItem("counterValue");
+  //   if (valueAsString) {
+  //     let newValue = JSON.parse(valueAsString);
+  //     setValue(newValue);
+  //   }
+  // }, []);
 
   const incrementHandler = () => {
     setValue(value + 1);
   };
 
-  const setToLocalStorageHandler = () => {
-    localStorage.setItem("counterValue", JSON.stringify(value));
-    localStorage.setItem("counterValue2", JSON.stringify(value + 1));
-  };
-
-  const getFromLocalStorageHandler = () => {
-    // setValue(JSON.parse(localStorage.getItem("counterValue")!))возможны ошибки, если NULLне будет показано в UI вообще ничего
-    let valueAsString = localStorage.getItem("counterValue");
-    if (valueAsString) {
-      let newValue = JSON.parse(valueAsString);
-      setValue(newValue);
-    }
-  };
-
-  const clearLocalStorageHandler = () => {
-    localStorage.clear();
-    setValue(0);
-  };
-
-  const removeItemFromLocalStorageHandler = () => {
-    localStorage.removeItem("counterValue2");
-  };
   return (
     <div className="App">
       <h1>{value}</h1>
       <button onClick={incrementHandler}>increment</button>
-      <button onClick={setToLocalStorageHandler}>setTolocalStorage</button>
-      <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-      <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
-      <button onClick={removeItemFromLocalStorageHandler}>
-        removeFromLocalStorage
-      </button>
     </div>
   );
 }
