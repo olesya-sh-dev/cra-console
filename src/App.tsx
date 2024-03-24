cc933bc737efec1fe617e771fd0f7df4a4a788a2
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-03-24 18:49:10 +0300

    localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const incrementHandler = () => {
+    setValue(value + 1);
+  };
+
+  const setToLocalStorageHandler = () => {
+    localStorage.setItem("counterValue", JSON.stringify(value));
+    localStorage.setItem("counterValue2", JSON.stringify(value + 1));
+  };
+
+  const getFromLocalStorageHandler = () => {
+    // setValue(JSON.parse(localStorage.getItem("counterValue")!))возможны ошибки, если NULLне будет показано в UI вообще ничего
+    let valueAsString = localStorage.getItem("counterValue");
+    if (valueAsString) {
+      let newValue = JSON.parse(valueAsString);
+      setValue(newValue);
+    }
+  };
+
+  const clearLocalStorageHandler = () => {
+    localStorage.clear();
+    setValue(0);
+  };
+
+  const removeItemFromLocalStorageHandler = () => {
+    localStorage.removeItem("counterValue2");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        hello world hello world hello world NEW BRANCH byyyy
-        <p>jffk</p>
-      </header>
+      <h1>{value}</h1>
+      <button onClick={incrementHandler}>increment</button>
+      <button onClick={setToLocalStorageHandler}>setTolocalStorage</button>
+      <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+      <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
+      <button onClick={removeItemFromLocalStorageHandler}>
+        removeFromLocalStorage
+      </button>
     </div>
   );
 }
